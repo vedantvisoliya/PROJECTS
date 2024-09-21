900976305c1aaff93dc0f550b50485d9af5e7da6
Vedant Kumar <vedantvisoliya@gmail.com>
2024-09-21 15:53:35 +0530

    Create minM.py
    this program can find the smallest number in a list

---
+++ minM.py
@@ -0,0 +1,17 @@
+# creating a function which can find the minimum number in the list
+numbers = [560,450,456,767,345,987,634,589,238,189,899]
+
+def minimum_num(num_list):
+    mini_num = 0
+    num_list_length = len(num_list)
+    for nums in range(0,num_list_length):
+        if(nums == 0):
+            mini_num = num_list[nums]
+        elif(mini_num > num_list[nums]):
+            mini_num = num_list[nums]
+        
+        if(nums == (num_list_length-1)):
+            return mini_num
+
+
+print(minimum_num(numbers))
