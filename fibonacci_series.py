baa74b6197fd8ab6a3df76b99acdca8eeefa774c
Vedant Kumar <vedantvisoliya@gmail.com>
2024-09-24 12:05:51 +0530

    Update fibonacci_series.py
    completed the program

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,2 +1,16 @@
 print("....FIBONACCI SERIES....")
 num = int(input("enter the length of seires: "))
+
+def fibonacci(num):
+    fibo_list = []
+    for i in range(0,num+1):
+
+        if(i<2):
+            fibo_list.insert(i,i)
+        else:
+            fibo_var = fibo_list[i-1] + fibo_list[i-2]
+            fibo_list.insert(i,fibo_var)
+    
+    return fibo_list
+
+print(fibonacci(num))
